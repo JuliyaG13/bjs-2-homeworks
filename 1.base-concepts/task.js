4df9c5a93ef5215bf8ed020087d56fd878028af5
Yuliya Gumitova <yulya1397@mail.ru>
2024-08-31 15:11:40 +0300

    Первое задание полностью готово

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,37 @@
-"use strict"
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	let arr = [];
+	const discriminant = Math.pow(b, 2) - 4 * a * c;
+
+	if (discriminant < 0) {
+		return arr;
+	} else if (discriminant === 0) {
+		const root = -b / (2 * a);
+		arr.push(root);
+	} else {
+		const root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+		const root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+		arr.push(root1, root2);
+	}
+
+	return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+
+	let r = percent / 100 / 12;
+	let A = amount - contribution;
+
+	if (A <= 0) {
+		return 'Сумма кредита должна быть больше нуля.';
+	}
+
+	let n = countMonths;
+
+	const monthlyPayment = (A * r * Math.pow(1 + r, n)) / (Math.pow(1 + r, n) - 1);
+
+	const totalSum = (monthlyPayment * n).toFixed(2);
+
+	return totalSum;
 }
